eaf48f2bbc2fa57486f034cad61ee2e37443d269
lepperson11 <lepperson11@gmail.com>
2024-05-15 14:28:01 -0600

    Finished Project

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.static("./public"));
 app.use(express.json());
@@ -11,8 +12,8 @@ app.use(express.json());
 app.use("/api/v1/tasks", tasks);
 
 app.use(notFound);
-
-const port = 3000;
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
