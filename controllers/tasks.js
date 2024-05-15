b176b9a01421e4c8b1e5c7aa40ee1af7708d1f31
lepperson11 <lepperson11@gmail.com>
2024-05-15 12:17:23 -0600

    first commit

---
+++ controllers/tasks.js
@@ -0,0 +1,34 @@
+const Task = require("../models/Task");
+
+const getAllTasks = (req, res) => {
+  res.send("Get all tasks");
+};
+
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.send("Update Task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Delete Task");
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
